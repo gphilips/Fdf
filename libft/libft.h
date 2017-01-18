589bed87f37f7b820e71446d62274a31a9fe018e
Greg Philips <gregphilips@MacBook-Pro-de-Greg.local>
2017-01-18 12:23:23 +0100

    ajout libft

--- libft/libft.h
+++ libft/libft.h
@@ -14,6 +14,7 @@
 # define LIBFT_H
 
 # include <string.h>
+# define BUFF_SIZE 42
 
 void				*ft_memset(void *b, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
@@ -67,12 +68,14 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(const char *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
+
 typedef	struct		s_list
 {
 	void			*content;
 	size_t			content_size;
 	struct s_list	*next;
 }					t_list;
+
 t_list				*ft_lstnew(void const *content, size_t content_size);
 void				ft_lstdelone(t_list **alst, void(*del)(void *, size_t));
 void				ft_lstdel(t_list **alst, void (*del)(void *, size_t));
@@ -84,4 +87,14 @@ int					ft_isupper(int c);
 int					ft_islower(int c);
 void				ft_swap(int *a, int *b);
 int					ft_max(int *tab, unsigned int len);
+
+typedef struct		s_gnl
+{
+	int				fd;
+	char			*str;
+	char			*tmp;
+	struct s_gnl	*next;
+}					t_gnl;
+
+int					get_next_line(const int fd, char **line);
 #endif
